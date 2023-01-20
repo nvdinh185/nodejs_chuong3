decdd817a2bd5b23e050952e8c68090fa0984abe
nvdinh185 <nvdinh0766777123@gmail.com>
2023-01-20 21:13:25 +0700

    video 20 ok

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -17,12 +24,7 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, "resources/views"));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
+// Router init
+route(app);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
