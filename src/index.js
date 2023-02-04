972b01ee064dee9bc18741446945c826a38b83f0
nvdinh185 <nvdinh0766777123@gmail.com>
2023-02-04 16:13:23 +0700

    x

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
-const app = express();
-const port = 3000;
 
 const route = require('./routes');
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,7 +27,7 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', "hbs");
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Router init
