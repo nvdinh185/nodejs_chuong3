62e96c498a30d75d63a528b0cbbf29c60aa38c9e
nvdinh185 <nvdinh0766777123@gmail.com>
2023-05-03 11:47:10 +0700

    new dinh branch

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,12 +1,7 @@
 class NewsController {
     // [GET] /news
     index(req, res) {
-        res.render('news');
-    }
-
-    // [GET] /news/:slug
-    show(req, res) {
-        res.send('DEtail...');
+        res.send('news...');
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
-const path = require('path');
-
 const route = require('./routes');
-
 const app = express();
-const port = 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
-app.use(express.json());
-
-// HTTP logger
-app.use(morgan('combined'));
-
-// Template engine
-app.engine(
-    'hbs',
-    engine({
-        extname: '.hbs',
-    }),
-);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
 
 // Router init
 route(app);
 
-app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`),
-);
+// start server
+const port = 3000;
+app.listen(port, () => console.log(`Server is starting on port ${port}...`));
